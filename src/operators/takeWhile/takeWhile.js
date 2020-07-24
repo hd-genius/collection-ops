50b94fbe6113d5b94a9e6c8c808a92b60aa1a347
Roy Stewart <roythomasstewart@gmail.com>
2020-07-23 20:21:36 -0400

    Corrected the return type specified in the takeWhile doc comment

--- src/operators/takeWhile/takeWhile.js
+++ src/operators/takeWhile/takeWhile.js
@@ -3,7 +3,7 @@
  * @function takeWhile
  * @template T
  * @param {Predicate<T>} predicate
- * @returns {Generator<T>} a generator that will emit values until a value does not fulfill the predicate
+ * @returns {Operator<T>} a generator that will emit values until a value does not fulfill the predicate
  */
 module.exports = predicate => function*(source) {
     for (const value of source) {
