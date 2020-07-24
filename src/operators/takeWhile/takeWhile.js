cef5a24e1fdf35ddfd59de916fb94074e85c17cb
Roy Stewart <roythomasstewart@gmail.com>
2020-07-23 20:20:03 -0400

    Added doc comment for the takeWhile function

--- src/operators/takeWhile/takeWhile.js
+++ src/operators/takeWhile/takeWhile.js
@@ -1,3 +1,10 @@
+/**
+ *
+ * @function takeWhile
+ * @template T
+ * @param {Predicate<T>} predicate
+ * @returns {Generator<T>} a generator that will emit values until a value does not fulfill the predicate
+ */
 module.exports = predicate => function*(source) {
     for (const value of source) {
         if (predicate(value)) {
