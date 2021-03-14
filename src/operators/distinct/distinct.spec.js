707fd7f15334d701736a09e303f4428c99518315
Roy Stewart <roythomasstewart@gmail.com>
2021-03-14 01:42:33 -0500

    Added better tests to the distinct method

--- src/operators/distinct/distinct.spec.js
+++ src/operators/distinct/distinct.spec.js
@@ -1,6 +1,23 @@
 const { distinct } = require('collection-ops');
+const { testCasesForData } = require('../../test-utils');
 
 describe('distinct', () => {
+    testCasesForData([1, 1, 2, 2, 3, 3])('should only return one instance of a given value', (type, data) => {
+        const result = distinct(data);
+
+        expect(result.next().value).toBe(1);
+        expect(result.next().value).toBe(2);
+        expect(result.next().value).toBe(3);
+    });
+
+    testCasesForData([1, 2, 1, 3, 3, 2])('should return each value in the order that they are first found', (type, data) => {
+        const result = distinct(data);
+
+        expect(result.next().value).toBe(1);
+        expect(result.next().value).toBe(2);
+        expect(result.next().value).toBe(3);
+    });
+
     it('should be reusable', () => {
         const dataSource = [1, 2, 3];
 
@@ -9,5 +26,5 @@ describe('distinct', () => {
             const currentValue = firstUsage.next().value;
             expect(currentValue).toBe(i);
         }
-    })
-})
+    });
+});
