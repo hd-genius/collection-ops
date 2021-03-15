d144290d8fe5480751a59e201616eccb6cb2e210
Roy Stewart <roythomasstewart@gmail.com>
2021-03-15 16:47:09 -0400

    moved the aggregator jsdoc types to the types.js file

--- src/aggregators/aggregators.js
+++ src/aggregators/aggregators.js
@@ -1,24 +1,4 @@
 /**
- * A function that takes an iterable and aggregates it to create a single result.
- * Cannot be used with infinite collections, it will cause an infinite loop.
- * @callback Aggregator
- * @template T, U
- * @param {Iterable<T>} source an iterable to be aggregated
- * @returns {U} a single value that is the result of aggregating a source
- */
-
-/**
- * A function that compares two arguments
- * @callback Comparator
- * @template T
- * @param {T} first
- * @param {T} second
- * @returns {number} a negative number if the first argument is smaller,
- * zero if both arguments are equal, or a positive number if the
- * first argument is larger than the second
- */
-
-/**
  * @function
  * @template T
  * @param {Comparator<T>} comparator the comparator used to find the largest value
