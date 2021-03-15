82420a8bbc354feac713ade7cbde12ad09a13099
Roy Stewart <roythomasstewart@gmail.com>
2021-03-15 16:12:21 -0400

    Made the take while operation reusable

--- src/operators/takeWhile/takeWhile.js
+++ src/operators/takeWhile/takeWhile.js
@@ -1,3 +1,5 @@
+const reusable = require("../../helpers/reusable/reusable");
+
 /**
  *
  * @function takeWhile
@@ -5,12 +7,14 @@
  * @param {Predicate<T>} predicate
  * @returns {Operator<T>} a generator that will emit values until a value does not fulfill the predicate
  */
-module.exports = predicate => function*(source) {
-    for (const value of source) {
-        if (predicate(value)) {
-            yield value;
-        } else {
-            break;
+module.exports = predicate => reusable(
+    function*(source) {
+        for (const value of source) {
+            if (predicate(value)) {
+                yield value;
+            } else {
+                break;
+            }
         }
     }
-};
+);
--- src/operators/takeWhile/takeWhile.spec.js
+++ src/operators/takeWhile/takeWhile.spec.js
@@ -1,14 +1,14 @@
 const { takeWhile } = require('collection-ops');
+const { testThatTheResultIsReusable } = require('../../test-utils');
 
 describe('takeWhile', () => {
+    testThatTheResultIsReusable(takeWhile(x => x < 3)([1, 2, 3]))
+
     it('should return an operation that stops emitting values once the predicate returns false', () => {
         const testPredicate = jest.fn();
         testPredicate.mockReturnValueOnce(true).mockReturnValueOnce(false);
         const dataSource = [1, 2, 3];
         const result = takeWhile(testPredicate)(dataSource);
-        expect(result.next().value).toBe(1);
-        expect(result.next().done).toBeTruthy();
+        expect(result).toHaveValues(1);
     });
-
-    it('should create a reusable operation', () => {})
 });
