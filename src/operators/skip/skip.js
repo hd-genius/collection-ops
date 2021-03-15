d463e88436158fa6147c01c4057bb85a59a5b167
Roy Stewart <roythomasstewart@gmail.com>
2021-03-15 16:28:15 -0400

    Made the iterable result of the skip operation reusable

--- src/operators/skip/skip.js
+++ src/operators/skip/skip.js
@@ -1,10 +1,14 @@
-exports.skip = count => function*(source) {
-    let amountToDrop = count;
-    for (const value of source) {
-        if (amountToDrop > 0) {
-            amountToDrop--;
-        } else {
-            yield value;
+const reusable = require("../../helpers/reusable/reusable");
+
+exports.skip = count => reusable(
+        function*(source) {
+        let amountToDrop = count;
+        for (const value of source) {
+            if (amountToDrop > 0) {
+                amountToDrop--;
+            } else {
+                yield value;
+            }
         }
     }
-};
+);
--- src/operators/skip/skip.spec.js
+++ src/operators/skip/skip.spec.js
@@ -1,21 +1,17 @@
 const { skip } = require('collection-ops');
-const { testCasesForData } = require('../../test-utils');
+const { testCasesForData, testThatTheResultIsReusable } = require('../../test-utils');
 
 describe('skip', () => {
+    testThatTheResultIsReusable(skip(2)([1, 2, 3, 4, 5]));
+
     testCasesForData([1, 2, 3, 4, 5])('should skip the requested number of values in the source when given a(n) %s', (type, dataSource) => {
         const result = skip(2)(dataSource);
-        for (let i = 3; i <= 5; i++) {
-            expect(result.next().value).toEqual(i);
-        }
-        expect(result.next().done).toBeTruthy();
+        expect(result).toHaveValues(3, 4, 5);
     });
 
     testCasesForData([1, 2, 3, 4, 5])('should not skip any values when given a number less than or equal to 0 when given a(n) %s', (type, dataSource) => {
         const result = skip(-1)(dataSource);
-        for (let i = 1; i <= 5; i++) {
-            expect(result.next().value).toEqual(i);
-        }
-        expect(result.next().done).toBeTruthy();
+        expect(result).toHaveValues(1, 2, 3, 4, 5);
     });
 
     it('should create an operation that can be reused multiple times', () => {
@@ -23,15 +19,9 @@ describe('skip', () => {
         const dropOperation = skip(3);
 
         const firstUsage = dropOperation(dataSource);
-        for (let i = 4; i <= 5; i++) {
-            expect(firstUsage.next().value).toEqual(i);
-        }
-        expect(firstUsage.next().done).toBeTruthy();
+        expect(firstUsage).toHaveValues(4, 5);
 
         const secondUsage = dropOperation(dataSource);
-        for (let i = 4; i <= 5; i++) {
-            expect(secondUsage.next().value).toEqual(i);
-        }
-        expect(secondUsage.next().done).toBeTruthy();
+        expect(secondUsage).toHaveValues(4, 5);
     });
 });
