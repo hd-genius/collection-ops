896a60a28cc4e8bba417aaff640641a940e482a6
Roy Stewart <roythomasstewart@gmail.com>
2021-03-15 16:16:32 -0400

    Made the result of the sort operation reusable

--- src/operators/sort/sort.js
+++ src/operators/sort/sort.js
@@ -1,3 +1,5 @@
+const reusable = require("../../helpers/reusable/reusable");
+
 /**
  * Cannot be used with infinite collections, it will cause an infinite loop.
  * Is not entirely lazily evaluated, all values are pulled from source
@@ -8,18 +10,20 @@
  * @returns {Operator<T, T>} an Operator that returns values from source in
  * the order of largest to smallest using comparator.
  */
-module.exports = comparator => function*(source) {
-    let remainingValues = Array.from(source);
-    while (remainingValues.length > 0) {
-        for (let index = remainingValues.length; index > 0; index--) {
-            let currentValue = remainingValues[index];
-            let nextIndex = index - 1;
-            let nextValue = remainingValues[nextIndex];
-            if (comparator(currentValue, nextValue) < 0) {
-                remainingValues[index] = nextValue;
-                remainingValues[nextIndex] = currentValue;
+module.exports = comparator => reusable(
+    function*(source) {
+        let remainingValues = Array.from(source);
+        while (remainingValues.length > 0) {
+            for (let index = remainingValues.length; index > 0; index--) {
+                let currentValue = remainingValues[index];
+                let nextIndex = index - 1;
+                let nextValue = remainingValues[nextIndex];
+                if (comparator(currentValue, nextValue) < 0) {
+                    remainingValues[index] = nextValue;
+                    remainingValues[nextIndex] = currentValue;
+                }
             }
+            yield remainingValues.shift();
         }
-        yield remainingValues.shift();
     }
-}
+);
--- src/operators/sort/sort.spec.js
+++ src/operators/sort/sort.spec.js
@@ -1,8 +1,10 @@
-const { testCasesForData } = require('../../test-utils');
+const { testCasesForData, testThatTheResultIsReusable } = require('../../test-utils');
 const { sort } = require('collection-ops');
 
 
 describe('sort', () => {
+    testThatTheResultIsReusable(sort((x, y) => x - y)([1, 5, 4, 2, 3]));
+
     testCasesForData([4, 2, 3, 5, 1])('should create an operation that sorts values in the correct order when given a(n) %s', (type, dataSource) => {
         const comparator = (x, y) => x - y;
 
