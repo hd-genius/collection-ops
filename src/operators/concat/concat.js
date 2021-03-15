76da0b47b24c88726ba71453e85a026cc3fe8eda
Roy Stewart <roythomasstewart@gmail.com>
2021-03-15 16:58:24 -0400

    Cleaned up the concat operation file

--- src/operators/concat/concat.js
+++ src/operators/concat/concat.js
@@ -1,9 +1,9 @@
 const reusable = require('../../helpers/reusable/reusable');
 
-function* concat(...sources) {
-    for (const source of sources) {
-        yield *source;
+module.exports = reusable(
+    function* concat(...sources) {
+        for (const source of sources) {
+            yield *source;
+        }
     }
-};
-
-module.exports = reusable(concat);
+);
