16172d982b5a678f99a425e0544e62ce15f97568
Roy Stewart <roythomasstewart@gmail.com>
2021-03-15 16:36:08 -0400

    Cleaned up the tests for the limit operation

--- src/operators/limit/limit.spec.js
+++ src/operators/limit/limit.spec.js
@@ -5,15 +5,12 @@ const { testCasesForData } = require('../../test-utils');
 describe('limit', () => {
     testCasesForData([1, 2, 3])('should only return the first number of values that were asked for when given a(n) %s', (type, dataSource) => {
         const result = limit(3)(dataSource);
-        for (let i = 0; i < 3; i++) {
-            result.next();
-        }
-        expect(result.next().done).toBeTruthy();
+        expect(result).toHaveValues(1, 2, 3);
     });
 
     testCasesForData([1, 2, 3])('should return a generator that does not emit any values if the amountToTake parameter is <= 0 when given a(n) %s', (type, datasource) => {
         const result = limit(-1)(datasource);
-        expect(result.next().done).toBeTruthy();
+        expect(result).toHaveValues();
     })
 
     it('should create an operation that can be reused multiple times', () => {
@@ -21,15 +18,9 @@ describe('limit', () => {
         const takeOperation = limit(3);
         
         const firstUsage = takeOperation(dataSource);
-        for (let i = 1; i <= 3; i++) {
-            expect(firstUsage.next().value).toEqual(i);
-        }
-        expect(firstUsage.next().done).toBeTruthy();
+        expect(firstUsage).toHaveValues(1, 2, 3);
 
         const secondUsage = takeOperation(dataSource);
-        for (let i = 1; i <= 3; i++) {
-            expect(secondUsage.next().value).toEqual(i);
-        }
-        expect(secondUsage.next().done).toBeTruthy();
+        expect(secondUsage).toHaveValues(1, 2, 3);
     });
 });
