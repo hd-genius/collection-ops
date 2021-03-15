92e8eeebf05f3dc4c872bcbde8b0dcee4845cb3f
Roy Stewart <roythomasstewart@gmail.com>
2021-03-15 16:24:02 -0400

    Made the result of the skipWhile operation reusable

--- src/operators/skipWhile/skipWhile.js
+++ src/operators/skipWhile/skipWhile.js
@@ -1,11 +1,15 @@
-exports.skipWhile = predicate => function*(source) {
-    let hasFailed = false;
-    for (const value of source) {
-        hasFailed = hasFailed || !predicate(value);
-        if (hasFailed) {
-            yield value;
-        } else {
-            continue;
+const reusable = require("../../helpers/reusable/reusable");
+
+exports.skipWhile = predicate => reusable(
+    function*(source) {
+        let hasFailed = false;
+        for (const value of source) {
+            hasFailed = hasFailed || !predicate(value);
+            if (hasFailed) {
+                yield value;
+            } else {
+                continue;
+            }
         }
     }
-}
+);
--- src/operators/skipWhile/skipWhile.spec.js
+++ src/operators/skipWhile/skipWhile.spec.js
@@ -1,12 +1,13 @@
 const { skipWhile } = require('collection-ops');
-const { testCasesForData } = require('../../test-utils');
+const { testCasesForData, testThatTheResultIsReusable } = require('../../test-utils');
 
 describe('skipWhile', () => {
+    testThatTheResultIsReusable(skipWhile(x => x < 3)([1, 2, 3, 4]));
+
     testCasesForData([1, 2, 3, 4])('should not emit any values until the predicate fails when given a(n) %s', (type, data) => {
         const result = skipWhile(x => x < 3)(data);
 
-        expect(result.next().value).toBe(3);
-        expect(result.next().value).toBe(4);
+        expect(result).toHaveValues(3, 4);
     });
 
     it('should not emit any values if the predicate never succeeds', () => {});
