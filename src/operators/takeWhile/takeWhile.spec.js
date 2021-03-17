4527a62377b33a1ef1045f00d702bc0bce071c84
Roy Stewart <roythomasstewart@gmail.com>
2021-03-17 19:03:28 -0400

    Added more tests for the takeWhile function

--- src/operators/takeWhile/takeWhile.spec.js
+++ src/operators/takeWhile/takeWhile.spec.js
@@ -4,11 +4,17 @@ const { testThatTheResultIsReusable, testThatTheParametersAreNotModified, testCa
 describe('takeWhile', () => {
     testThatTheResultIsReusable(takeWhile(x => x < 3)([1, 2, 3]));
 
-    it('should return an operation that stops emitting values once the predicate returns false', () => {
+    testThatTheParametersAreNotModified(takeWhile(x => x < 2), [1, 2, 3]);
+
+    testCasesForData([1, 2, 3])('should return an operation that stops emitting values once the predicate returns false when given a(n) %s', (type, data) => {
         const testPredicate = jest.fn();
         testPredicate.mockReturnValueOnce(true).mockReturnValueOnce(false);
-        const dataSource = [1, 2, 3];
-        const result = takeWhile(testPredicate)(dataSource);
+        const result = takeWhile(testPredicate)(data);
+        expect(result).toHaveValues(1);
+    });
+
+    testCasesForData([1, 8, 3, 4, 2])('should not emit values after the predicate fails when given a(n) %s', (type, data) => {
+        const result = takeWhile(x => x < 6)(data);
         expect(result).toHaveValues(1);
     });
 });
