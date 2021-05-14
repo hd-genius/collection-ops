fe43d6d73c647048d3eae654d437042d80e0148e
Roy Stewart <roythomasstewart@gmail.com>
2021-05-14 06:58:25 -0400

    Added a documentation comment to the find Aggregator

--- src/aggregators/find/find.js
+++ src/aggregators/find/find.js
@@ -1,3 +1,9 @@
+/**
+ * @function find
+ * @template T
+ * @param {Predicate<T>} predicate A Predicate function to check values against
+ * @return {Aggregator<T, T>} An aggregator that returns the first value in an iterable that satisfies the predicate
+ */
 module.exports = predicate => source => {
     for (const value of source) {
         if (predicate(value)) {
