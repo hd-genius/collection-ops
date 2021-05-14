efc9beb917ddf90636c9e411fae198a1466c7934
Roy Stewart <roythomasstewart@gmail.com>
2021-05-14 06:57:04 -0400

    Added a documentation comment to the count aggregator

--- src/aggregators/count/count.js
+++ src/aggregators/count/count.js
@@ -1,3 +1,9 @@
+/**
+ * @function count
+ * @template T
+ * @param {Iterable<T>} source The iterable to count
+ * @return {number} The number of elements in the source
+ */
 module.exports = function(source) {
     let count = 0;
     for (const value of source) {
