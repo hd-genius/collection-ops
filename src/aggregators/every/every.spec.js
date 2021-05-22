8d31001f3a92eb8ac47ad85a60d0f0c41fce0be1
Roy Stewart <roythomasstewart@gmail.com>
2021-05-22 10:08:00 -0400

    Added some unit tests for the every aggregator

---
+++ src/aggregators/every/every.spec.js
@@ -0,0 +1,19 @@
+const { every } = require('collection-ops');
+const { testCasesForData } = require('../../test-utils');
+
+describe('every', () => {
+    testCasesForData([1, 2, 3])('should return true if every item in an %s passes the given predicate', (type, dataSource) => {
+        const result = every(x => true)(dataSource);
+        expect(result).toBeTruthy();
+    });
+
+    testCasesForData([1, 2, 3])('should return false if any item in an %s fails the given predicate', (type, dataSource) => {
+        const result = every(x => x < 3)(dataSource);
+        expect(result).toBeFalsy();
+    });
+
+    testCasesForData([])('should return true if there are no items in an $s', (type, dataSource) => {
+        const result = every(x => false)(dataSource);
+        expect(result).toBeTruthy();
+    });
+});
