6acdf4338a353a306033a04a896b5887f7634b58
Roy Stewart <roythomasstewart.com>
2026-02-28 21:43:35 -0500

    Add doc comment to more functions

--- src/aggregators/min.js
+++ src/aggregators/min.js
@@ -1,5 +1,11 @@
 const max = require('./max');
 
+/**
+ * @function
+ * @template T
+ * @param {Comparator<T>} comparator the comparator used to find the largest value
+ * @returns {Aggregator<T, T>} an Aggregator that finds the smallest value in the source
+ */
 module.exports = (comparator) => {
     const inverseComparator = (x) => 0 - comparator(x);
     return max(inverseComparator);
--- src/aggregators/some.js
+++ src/aggregators/some.js
@@ -1,3 +1,10 @@
+/**
+ * @function every
+ * @template T
+ * @param {Predicate<T>} predicate A Predicate function to check values against
+ * @returns {Aggregator<T, bool>} An aggregator that returns true if any element in the source returns true when passed
+ * to the predicate. Returns false if every element returns false.
+ */
 module.exports = (predicate) => (source) => {
     for (const value of source) {
         if (predicate(value)) {
